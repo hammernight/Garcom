db2d48848efb36224a9f7cd06186b15491eaabdf
Pedro Nascimento <pnascimento@gmail.com>
2011-10-14 14:42:58 -0300

    rails skeleton. GO OSCAR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Garcom::Application.config.secret_token = '2754f47acbf5b6634daceb36a093deb0a50a8b140d384e4900b606cd724056f8b5127017e17d1620a6f3090ffc989f6e675b4f005c1b5c6e038719effd1b3053'
